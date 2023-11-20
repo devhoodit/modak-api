5c8436a8796477dc514c8c6416d3ba61011e02f9
devhoodit <72541438+devhoodit@users.noreply.github.com>
2023-11-21 04:07:21 +0900

    token

---
+++ lib/src/token.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+Codec<String, String> stringToBase64 = utf8.fuse(base64);
+
+class Token {
+  final String uuid;
+  final DateTime exp;
+  final String _tokenString;
+  String get tokenString => _tokenString;
+
+  Token(this.uuid, this.exp, this._tokenString);
+
+  static Token parseFromString(String tokenString) {
+    final splitString = tokenString.split(".");
+    if (splitString.length != 2) {
+      throw Exception("Invalid token string");
+    }
+    final tokenBody = splitString[0];
+    final decodedBody = stringToBase64.decode(tokenBody);
+    Map<String, dynamic> jsonBody = jsonDecode(decodedBody);
+
+    final String uuid = jsonBody["uuid"];
+    final exp = DateTime.fromMillisecondsSinceEpoch(jsonBody["exp"] * 1000);
+
+    return Token(uuid, exp, tokenString);
+  }
+}
