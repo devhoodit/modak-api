1d84f4feeba72d826ee5baa52fcb8c581d8320c8
devhoodit <hoodit@naver.com>
2024-01-06 20:05:27 +0900

    auth

--- lib/src/api/auth.dart
+++ lib/src/api/auth.dart
@@ -1,11 +1,10 @@
 import 'package:modak/src/token.dart';
 
-class AuthorizeRequired {
-  final Token _token;
-  AuthorizeRequired(this._token);
-  Map<String, String>? addTokenHeader({Map<String, String>? header}) {
+class Auth {
+  static Map<String, String>? addTokenHeader(Token token,
+      {Map<String, String>? header}) {
     header ??= <String, String>{};
-    header["X-MD-Token"] = _token.tokenString;
+    header["X-MD-Token"] = token.tokenString;
     return header;
   }
 }
