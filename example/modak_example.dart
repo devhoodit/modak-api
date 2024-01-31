75ed76488e81e1349ea0381c68a2d7bcf8195602
devhoodit <hoodit@naver.com>
2024-01-31 22:38:48 +0900

    test init

--- example/modak_example.dart
+++ example/modak_example.dart
@@ -1,4 +1,18 @@
-import 'package:modak/modak.dart';
+import 'package:modak/src/api/endpoint.dart';
+import 'package:modak/src/api/request.dart';
+import 'package:modak/src/modak_base.dart';
 import 'package:modak/src/token.dart';
 
-void main() async {}
+void main() async {
+  var m = Modak(
+      token: Token.parseFromString("your access token"),
+      refreshToken: "your refresh token",
+      endpoint: Endpoint(host: "host", port: 1234));
+  try {
+    var collections = await m.collection.getCollectionsUUID(0, 100);
+    print(collections);
+  } on RequestAPIError catch (e) {
+    print(e.statusCode);
+    print(e.body);
+  }
+}
