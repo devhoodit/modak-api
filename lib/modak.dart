195ddecf2f2027b2122092a205ed572b3fcd310f
devhoodit <hoodit@naver.com>
2024-01-31 19:25:52 +0900

    modak

--- lib/modak.dart
+++ lib/modak.dart
@@ -5,5 +5,5 @@ library;
 
 export 'src/modak_base.dart';
 export 'src/token.dart' show Token;
-export 'src/api/collection.dart' show Collection, CollectionsUUID;
+export 'src/api/collection/collection_dto.dart';
 // TODO: Export any libraries intended for clients of this package.
