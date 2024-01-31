b7e9c7b6791b5f157c2f8e2c535d28e88c350f01
devhoodit <hoodit@naver.com>
2024-01-31 19:27:29 +0900

    api request helper

--- lib/src/api/request.dart
+++ lib/src/api/request.dart
@@ -2,36 +2,53 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:modak/modak.dart';
-import 'package:modak/src/api/auth.dart';
+import 'package:modak/src/api/auth/auth.dart';
+
+abstract interface class IAPIRequest {
+  Future<T> get<T, G>(String url, T Function(G json) task,
+      [Map<String, String>? headers]);
+  Future<T> post<T, G>(String url, T Function(G json) task,
+      [Object? body, Map<String, String>? headers]);
+}
 
 class AuthenticationError implements Exception {
   final String message;
   AuthenticationError(this.message);
 }
 
-class APIRequest {
-  APIRequest._();
-  static final APIRequest _instance = APIRequest._();
+class RequestAPIError implements Exception {
+  final int statusCode;
+  final String body;
+  RequestAPIError(this.statusCode, this.body);
+}
+
+class APIRequest implements IAPIRequest {
+  APIRequest._init();
+  static final APIRequest _instance = APIRequest._init();
   factory APIRequest() => _instance;
-  static Future<T> getWithToken<T, G>(
-      String url, T Function(G json) task, Token token,
+  Future<T> getWithToken<T, G>(String url, T Function(G json) task, Token token,
       [Map<String, String>? headers]) async {
-    final reqheaders = Auth.addTokenHeader(token, header: headers);
+    final reqheaders = addTokenHeader(token, header: headers);
     try {
-      return await APIRequest.get(url, task, reqheaders);
-    } on AuthenticationError catch (e) {
-      if (e.message == "Token is expired") {
-        // token refresh needed
-        final reqheaders = Auth.addTokenHeader(token, header: headers);
-        return await APIRequest.get(url, task, reqheaders);
-      }
-      rethrow;
+      return await get(url, task, reqheaders);
     } catch (e) {
       rethrow;
     }
   }
 
-  static Future<T> get<T, G>(String url, T Function(G json) task,
+  Future<T> postWithToken<T, G>(
+      String url, T Function(G json) task, Token token,
+      [Object? body, Map<String, String>? headers]) async {
+    final reqheaders = addTokenHeader(token, header: headers);
+    try {
+      return await post(url, task, body, reqheaders);
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  @override
+  Future<T> get<T, G>(String url, T Function(G json) task,
       [Map<String, String>? headers]) async {
     final res = await http.get(Uri.parse(url), headers: headers);
     final j = json.decode(res.body);
@@ -41,7 +58,22 @@ class APIRequest {
       case 401:
         throw AuthenticationError(j["message"]);
       default:
-        throw Exception("status code: ${res.statusCode} body: ${res.body}");
+        throw RequestAPIError(res.statusCode, res.body);
+    }
+  }
+
+  @override
+  Future<T> post<T, G>(String url, T Function(G json) task,
+      [Object? body, Map<String, String>? headers]) async {
+    final res = await http.post(Uri.parse(url), headers: headers, body: body);
+    final j = json.decode(res.body);
+    switch (res.statusCode) {
+      case 200:
+        return task(j);
+      case 401:
+        throw AuthenticationError(j["message"]);
+      default:
+        throw RequestAPIError(res.statusCode, res.body);
     }
   }
 }
