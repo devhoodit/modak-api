3505700b65752a603ef6a6dfc952632133af3f78
devhoodit <hoodit@naver.com>
2024-03-01 23:21:32 +0900

    Feat: request multipart

--- lib/src/api/request.dart
+++ lib/src/api/request.dart
@@ -9,6 +9,8 @@ abstract interface class IAPIRequest {
       {Map<String, String>? headers});
   Future<T> post<T, G>(String url, T Function(G json) task,
       {Object? body, Map<String, String>? headers});
+  Future<T> multipart<T, G>(
+      String url, T Function(G json) task, List<http.MultipartFile> files);
 }
 
 class AuthenticationError implements Exception {
@@ -68,6 +70,29 @@ class APIRequest implements IAPIRequest {
     final res = await http.post(Uri.parse(url), headers: headers, body: body);
     final j = json.decode(res.body);
     switch (res.statusCode) {
+      case 200:
+        return task(j);
+      case 401:
+        throw AuthenticationError(j["message"]);
+      default:
+        throw RequestAPIError(res.statusCode, res.body);
+    }
+  }
+
+  @override
+  Future<T> multipart<T, G>(
+      String url, T Function(G json) task, List<http.MultipartFile> files,
+      {Map<String, String>? headers}) async {
+    final client = http.MultipartRequest('post', Uri.parse(url));
+    if (headers != null) {
+      client.headers.addAll(headers);
+    }
+    client.files.addAll(files);
+    final streamResponse = await client.send();
+    final res = await http.Response.fromStream(streamResponse);
+    print(res.body);
+    final j = json.decode(res.body);
+    switch (streamResponse.statusCode) {
       case 200:
         return task(j);
       case 401:
