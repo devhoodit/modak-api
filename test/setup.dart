4b8b147fdbdfd6b3ddc1af45184ad24dd0125443
devhoodit <hoodit@naver.com>
2024-03-02 00:19:17 +0900

    setup config file loader

---
+++ test/setup.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:yaml/yaml.dart';
+
+import 'setup_dto.dart';
+
+void main() {
+  final testConfig = loadTestConfig();
+  print(testConfig);
+}
+
+TestConfig loadTestConfig() {
+  final configFile = File("./test/test_config.yaml");
+  var config = loadYaml(configFile.readAsStringSync());
+  return TestConfig.fromJson(jsonDecode(jsonEncode(config)));
+}
