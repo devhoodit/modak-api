4d516404988f3571f1eda065dfe1fa39400e2eb5
devhoodit <hoodit@naver.com>
2024-05-12 23:58:42 +0900

    api request return value naming apires

--- lib/src/api/collection/collection.dart
+++ lib/src/api/collection/collection.dart
@@ -13,29 +13,34 @@ class CollectionAPI {
   Endpoint endpoint;
   CollectionAPI(this.auth, this.endpoint);
 
-  Future<List<String>> getCollectionsUUID(int offset, int limit) async {
-    final collectionsUUID = await auth.get(
-        "${endpoint.baseurl}/collection?offset=$offset&limit=$limit",
+  /// get collection uuids by [user], [offset] and [limit]
+  Future<List<String>> getCollectionsUUID(
+      String user, int offset, int limit) async {
+    final apires = await auth.get(
+        "${endpoint.baseurl}/collection/list/$user?offset=$offset&limit=$limit",
         responseJsonWrapper(CollectionsUUID.fromJson));
-    return collectionsUUID.data.collections;
+    return apires.data.collections;
   }
 
+  /// get collection information by collection [uuid]
   Future<Collection> getCollectionByUUID(String uuid) async {
-    final collection = await auth.get("${endpoint.baseurl}/collection/$uuid",
+    final apires = await auth.get("${endpoint.baseurl}/collection/$uuid",
         responseJsonWrapper(Collection.fromJson));
-    return collection.data;
+    return apires.data;
   }
 
   ByteData _getByteData(http.Response res) {
     return ByteData.view(res.bodyBytes.buffer);
   }
 
+  /// get collection image [ByteData] by [uuid]
   Future<ByteData> getCollectionImage(String uuid) async {
     final res = await auth.get(
         "${endpoint.baseurl}/collection/image/$uuid", _getByteData);
     return res.data;
   }
 
+  /// post [collection] with image by [imagePath]
   Future<String> postCollection(String imagePath, Collection collection) async {
     var multipatFiles = <http.MultipartFile>[];
     final jsonData = json.encode(collection.toJson());
@@ -48,6 +53,7 @@ class CollectionAPI {
     return uuid.data;
   }
 
+  /// delete collection by collection [uuid]
   Future<void> deleteCollection(String uuid) async {
     await auth.delete("${endpoint.baseurl}/collection/$uuid");
   }
