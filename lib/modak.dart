e094309d7b5fbab9c4ed07837262882a7911cfc6
devhoodit <hoodit@naver.com>
2024-05-19 23:50:24 +0900

    expose object

--- lib/modak.dart
+++ lib/modak.dart
@@ -3,12 +3,17 @@
 /// More dartdocs go here.
 library;
 
-export 'src/modak_base.dart';
-export 'src/api/endpoint.dart';
+export 'src/modak_base.dart' show Modak;
+export 'src/api/endpoint.dart' show Endpoint;
 export 'src/token.dart' show Token;
-export 'src/api/collection/collection_dto.dart';
-export 'src/api/article/article_dto.dart';
-export 'src/api/social/social_dto.dart';
-export 'src/api/errors.dart';
-export 'src/types/uuid.dart';
+export 'src/api/request.dart'
+    show RequestAPIError, AuthenticationError, APIResponse;
+export 'src/api/collection/collection_dto.dart'
+    show Collection, CollectionsUUID, GeoLocation;
+export 'src/api/article/article_dto.dart'
+    show Article, ArticleLinks, ArticleMeta, PostArticle, OrderInfo;
+export 'src/api/social/social_dto.dart'
+    show Followers, Followings, FollowRequest, FollowRequests, Relation;
+export 'src/api/errors.dart' show InvalidInputRangeError, InvalidInputError;
+export 'src/types/uuid.dart' show UUID;
 // TODO: Export any libraries intended for clients of this package.
