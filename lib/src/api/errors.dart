6201f37ed22aec98eb57d6a3398aaff513612617
devhoodit <hoodit@naver.com>
2024-05-19 18:30:23 +0900

    validate offset, limit range

--- lib/src/api/errors.dart
+++ lib/src/api/errors.dart
@@ -2,3 +2,12 @@ class InvalidInputError implements Exception {
   final String message;
   InvalidInputError(this.message);
 }
+
+class InvalidInputRangeError extends InvalidInputError {
+  final int actualOffset;
+  final int actualLimit;
+  final int maxLimit;
+  InvalidInputRangeError(this.actualOffset, this.actualLimit, this.maxLimit)
+      : super(
+            "offset: $actualOffset, limit: $actualLimit, expected value offset >= 0, 1 <= limit <= $maxLimit");
+}
--- lib/src/api/validator.dart
+++ lib/src/api/validator.dart
@@ -3,17 +3,8 @@ import 'package:modak/src/api/errors.dart';
 /// validate [offset] and [limit] is in condition
 /// [offset] >=0 and 0 < [limit] <= maxlimit
 /// if not validate throw [InvalidInputError] with description
-validateRange(int offset, int limit, int maxLimit) {
-  if (offset < 0) {
-    throw InvalidInputError(
-        "offset cannot be $offset, must greater or equal to 0");
-  }
-  if (limit < 1) {
-    throw InvalidInputError(
-        "limit cannot be $limit, must greater then 0 and less then $maxLimit");
-  }
-  if (limit > maxLimit) {
-    throw InvalidInputError(
-        "limit cannot be $limit, must greater then 0 and less then $maxLimit");
+void validateRange(int offset, int limit, int maxLimit) {
+  if (offset < 0 || limit < 1 || limit > maxLimit) {
+    throw InvalidInputRangeError(offset, limit, maxLimit);
   }
 }
